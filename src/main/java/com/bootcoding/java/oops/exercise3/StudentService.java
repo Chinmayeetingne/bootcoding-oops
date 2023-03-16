2fdc2c2c666d7671e05b607e4b7909ded4255eab
Chinmayeetingne <rashitingne12@gmail.com>
2023-03-16 14:02:03 +0530

    assignment

---
+++ src/main/java/com/bootcoding/java/oops/exercise3/StudentService.java
@@ -0,0 +1,15 @@
+package com.bootcoding.java.oops.exercise3;
+
+public class StudentService {
+     public static Student[] findStudentAboveCriteriaOfPer(Student[] students, float criteriaOfPer) {
+          int count = 0;
+          Student[] s = new Student[10];
+          for (int i = 0; i < students.length; i++) {
+               if (students[i].percentage > criteriaOfPer)
+                    s[count++] = students[i];
+               }
+
+               return s;
+          }
+     }
+
