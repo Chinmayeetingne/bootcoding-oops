fad931de8cb1630d716b7083e5263652c38dd4ba
Chinmayeetingne <rashitingne12@gmail.com>
2023-03-16 14:01:45 +0530

    assignment

---
+++ src/main/java/com/bootcoding/java/oops/exercise3/Student.java
@@ -0,0 +1,11 @@
+package com.bootcoding.java.oops.exercise3;
+
+import java.security.SecureRandom;
+
+public class Student {
+    String name;
+    int rollNo;
+    float percentage;
+    String address;
+
+}
